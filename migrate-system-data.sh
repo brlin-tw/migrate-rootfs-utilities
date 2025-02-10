2bea87b329e2565ef11636fda385b16bc40841e2
林博仁(Buo-ren Lin) <buo.ren.lin@gmail.com>
2025-02-11 04:14:17 +0800

    fix(system): Re-enable WireGuard configuration syncing
    Signed-off-by: 林博仁(Buo-ren Lin) <buo.ren.lin@gmail.com>

--- migrate-system-data.sh
+++ migrate-system-data.sh
@@ -3,7 +3,10 @@
 #
 # Copyright 2025 林博仁(Buo-ren Lin) <buo.ren.lin@gmail.com>
 # SPDX-License-Identifier: AGPL-3.0-or-later
-DESTINATION_ADDR=root@brlin-Vostro-5481.local
+DESTINATION_ROOTFS_SPEC="${DESTINATION_ROOTFS_SPEC:-unset}"
+
+ENABLE_SYNC_WIREGUARD_CONFIG="${ENABLE_SYNC_WIREGUARD_CONFIG:-true}"
+
 COMMON_RSYNC_OPTIONS=(
     --archive
     --acls
@@ -49,6 +52,35 @@ init(){
         exit 1
     fi
 
+    if test "${DESTINATION_ROOTFS_SPEC}" == unset; then
+        printf \
+            'Error: The DESTINATION_ROOTFS_SPEC parameter is not set.\n' \
+            1>&2
+        exit 1
+    fi
+
+    local regex_boolean_values='^(true|false)$'
+    local -a boolean_parameters=(
+        ENABLE_SYNC_WIREGUARD_CONFIG
+    )
+    local validate_failed=false
+    for param in "${boolean_parameters[@]}"; do
+        if ! [[ "${!param}" =~ ${regex_boolean_values} ]]; then
+            printf \
+                'Error: Invalid value of the boolean parameter %s(%s).\n' \
+                "${param}" \
+                "${!param}" \
+                1>&2
+            validate_failed=true
+        fi
+    done
+    if test "${validate_failed}" == true; then
+        printf \
+            'Error: Booleans parameter validation failed.\n' \
+            1>&2
+        exit 1
+    fi
+
     printf \
         'Info: Loading the functions file...\n'
     # shellcheck source-path=SCRIPTDIR
@@ -69,7 +101,17 @@ init(){
         exit 2
     fi
 
-#     sync_wireguard_configuration
+    if test "${ENABLE_SYNC_WIREGUARD_CONFIG}" == true; then
+        if ! sync_wireguard_configuration \
+            "${DESTINATION_ROOTFS_SPEC}" \
+            "${WIREGUARD_RSYNC_OPTIONS[@]}"; then
+            printf \
+                'Error: Unable to sync the WireGuard configuration files.\n' \
+                1>&2
+            exit 2
+        fi
+    fi
+
     #sync_udpraw_installation
 
     if ! end_timestamp="$(printf '%(%s)T')"; then
@@ -122,11 +164,24 @@ trap trap_err ERR
 }
 
 sync_wireguard_configuration(){
-    printf 'Info: Syncing WireGuard configuration files...\n'
-    rsync \
-        "${WIREGUARD_RSYNC_OPTIONS[@]}" \
-        /etc/wireguard \
-        "${DESTINATION_ADDR}:/etc/"
+    local destination_rootfs_spec="${1}"; shift 1
+    local -a rsync_options=("${@}"); set --
+
+    local wireguard_config_dir=/etc/wireguard
+    if ! test -e "${wireguard_config_dir}"; then
+        return 0
+    fi
+
+    printf 'Info: Syncing the WireGuard configuration files...\n'
+    if ! rsync \
+        "${rsync_options[@]}" \
+        "${wireguard_config_dir}" \
+        "${destination_rootfs_spec}/etc/"; then
+        printf \
+            'Error: Unable to sync the WireGuard configuration files.\n' \
+            1>&2
+        return 2
+    fi
 }
 
 sync_udpraw_installation(){
